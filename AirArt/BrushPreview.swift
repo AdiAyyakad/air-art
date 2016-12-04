4b120ff14ccd2b7345c6f4e51f8764cb0c2a90e0
Mark <markcreamer0@gmail.com>
2016-12-04 17:24:14 -0500

    Changed the preview so it looks more like a squiggle

--- AirArt/BrushPreview.swift
+++ AirArt/BrushPreview.swift
@@ -20,10 +20,20 @@ class BrushPreview: UIImageView {
             return
         }
         self.image?.draw(in: CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.frame.size.height))
-        
         context.clear(CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.frame.size.height))
-        context.move(to: CGPoint(x:self.frame.width/5,y:self.frame.height/2.0))
-        context.addLine(to: CGPoint(x:4*self.frame.width/5,y:self.frame.height/2.0))
+        
+        // Draw the squiggle
+        let numPoints = 20
+        let sx = self.frame.width/5
+        let sy = self.frame.height/2.0
+        var x = 0.0
+        var y = 0.0
+        context.move(to: CGPoint(x:sx,y:sy))
+        for index in 1...numPoints {
+            x = Double(sx)+Double(index)/Double(numPoints)*3.0*Double(self.frame.width/5.0)
+            y = Double(sy)+50.0*sin(Double(index)*2.0*M_PI/Double(numPoints))
+            context.addLine(to: CGPoint(x: x,y: y))
+        }
         
         context.setLineCap(.round)
         context.setLineWidth(paint.brushSize)
