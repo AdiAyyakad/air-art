86f7b6ddb281a4c025a4d87c13e3181ea23eac5a
Aditya Ayyakad <njadtiya@gmail.com>
2016-12-06 18:45:40 -0500

    Fixed typo

--- AirArt/SettingsTableViewController.swift
+++ AirArt/SettingsTableViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 enum ResuseIdentifier: String {
     case Tutorial = "Tutorial"
-    case Calibration = "Calibration"
+    case Calibration = "Calibrate"
 }
 
 class SettingsTableViewController: UITableViewController {
