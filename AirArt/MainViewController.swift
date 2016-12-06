71460d36241c0f59c3fdb06e9cc9e0e351b836f0
Mazi <mkalu@terpmail.umd.edu>
2016-12-06 13:00:38 -0500

    UI Design of AirArt
    I create a simple transition from tutorial to sketch view. I created
    buttons for Sketch View as well to mirror our invasion designs.

--- AirArt/MainViewController.swift
+++ AirArt/MainViewController.swift
@@ -9,9 +9,26 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    var happenOnce : Bool = true
+    
     @IBAction func didPressTutorial(_ sender: Any) {
         presentTutorial()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // Will happen just once
+        if(happenOnce) {
+            // I did not want to restructure your code, so I created a workaround
+            presentTutorial()
+            happenOnce = false
+        } else {
+            // Allowing us to view tutorial for the first time then present the sketch view if they 
+            // Select "Skip" or "Done"
+            
+            presentSketchView()
+        }
     }
 
 }
